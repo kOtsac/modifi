cfa00c1942141c3a7c044f0c22204b261d361397
Konstantin Torop <60583465+kOtsac@users.noreply.github.com>
2021-11-02 12:56:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
+import pyperclip as clipboard
 # Прграмма модификации информации
-
-# ((26+26+10+12)*(26+10+12)*(10+12)*(10))**4 = (74*48*22*10)**4  ??? 
-# хорошо бы (74)**16 но тут возможны проблемы c повторениями символов 
 
 
 # Основные переменные
 
 letters_down = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 letters_up =   ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-numerals = [0,1,2,3,4,5,6,7,8,9]
-symbols = ["!","@","#","$","%","^","&","*","(",")","_","+"]
+numerals = ["0","1","2","3","4","5","6","7","8","9"]
+symbols = ["!","@","#","$","%","^","&","*","(",")","_","+",".",","]
 list_of_lists = []
 lists_count = 0
 focus_count = 0
@@ -18,7 +16,7 @@ focus_list = []
 focus_letter = []
 lists_default = 0
 list_of_lists_default = []
-
+key_list = []
 
 
 
@@ -30,7 +28,7 @@ include_numerals = 1
 include_symbols = 1
 password_lenth = 15
 
-# добавления списков в список списков =]
+# добавления списков
 if include_letters_down == 1:
     list_of_lists_default.append(letters_down)
     lists_default  = lists_default + 1
@@ -47,8 +45,14 @@ if include_symbols == 1:
 
 
 #ввод ключей
-print("input key(number):")
-key = int(input())
+print("input key:")
+key = []
+key_input= (input())
+key_list = letters_up + letters_down + numerals + symbols
+
+for i in list(str(key_input)):
+    key.append(str(key_list.index(i)))
+key = int("".join(key))
 
 
 # алгоритм программы
@@ -64,15 +68,16 @@ for i in range (password_lenth):
     password.append (focus_letter)
     list_of_lists.remove (focus_list)
     lists_count = lists_count - 1
-# я знаю что это жесть, но я ещё не знаю язык настолько чтобы написать это в соответсвии с pep8
-# потому что там надо  добавлять ещё переменные, return, вот это вот всё. потом когда освою, тогода исправлю. 
-# как говорится "Работает? не трогай."
+
 
     if (lists_count == 0):
         lists_count = lists_default
         list_of_lists = list(list_of_lists_default)
-        
-password = [str(i) for i in password]
-print(("Password is"), "".join(password))
 
+
+
+
+
+password=("".join(password))
+clipboard.copy(password)
 password=[]
